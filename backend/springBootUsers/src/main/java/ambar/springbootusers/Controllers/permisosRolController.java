196ae8866bccdb9894e487d862dde2e7984cae53
Angel Garces <angellgarces1024@gmail.com>
2023-06-17 17:12:28 -0500

    validar

--- backend/springBootUsers/src/main/java/ambar/springbootusers/Controllers/permisosRolController.java
+++ backend/springBootUsers/src/main/java/ambar/springbootusers/Controllers/permisosRolController.java
@@ -1,5 +1,7 @@
 package ambar.springbootusers.Controllers;
 import ambar.springbootusers.Modelos.PermisosRol;
+import ambar.springbootusers.Modelos.rol;
+import ambar.springbootusers.Modelos.permiso;
 import ambar.springbootusers.Repositories.permisosRolRepository;
 import ambar.springbootusers.Repositories.permisosRepository;
 import ambar.springbootusers.Repositories.rolRepository;
@@ -47,12 +49,40 @@ public class permisosRolController {
         }
     }
 
-    @PutMapping("{Id}")
-    public PermisosRol updatePermisoRol(@PathVariable String _id, @RequestBody PermisosRol infoToUpdate){
+    @GetMapping("/validar-permiso/rol/{id}")
+    public PermisosRol validate(@PathVariable String id,@RequestBody  permiso permiso){
+
+
+        List<PermisosRol> permisosRoles = this.myPermisosRolRepo.getAllByRol(id);
+
+        System.out.println(permiso);
+
+        PermisosRol tienePermiso= null;
+
+        for( int i=0;i<permisosRoles.size();i++){
+            if(permisosRoles.get(i).getPermiso().getMetodo().equals(permiso.getMetodo()) & permisosRoles.get(i).getPermiso().getUrl().equals(permiso.getUrl())){
+
+                tienePermiso=permisosRoles.get(i);
+
+                break;
+            }
+        }
+
+        System.out.println(permisosRoles);
+
+        System.out.println(tienePermiso);
+        return tienePermiso;
+    }
+
+
+    @PutMapping("{Id}/rolID/{idRol}/permisoId/{idPermiso}")
+    public PermisosRol updatePermisoRol(@PathVariable String _id, @PathVariable String idRol, @PathVariable String idPermiso){
         PermisosRol search = this.myPermisosRolRepo.findById(_id).orElse(null);
-        if(search != null){
-            search.setPermiso(infoToUpdate.getPermiso());
-            search.setRol(infoToUpdate.getRol());
+        rol rolupdate = this.myRolRepository.findById(idRol).orElse(null);
+        permiso permisoUpdate = this.myPermisosRepo.findById(idPermiso).orElse(null);
+        if(search != null && rolupdate != null && permisoUpdate != null){
+            search.setRol(rolupdate);
+            search.setPermiso(permisoUpdate);
             return this.myPermisosRolRepo.save(search);
         }else throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Hace falta información para actualizar el permisoRol");
     }
